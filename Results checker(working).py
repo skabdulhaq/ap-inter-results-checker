5ad782f1d83c8dbfad2bf9af0a5b0ea2fe555da4
sk.abdul haq <75989562+skabdulhaq@users.noreply.github.com>
2021-08-28 17:26:02 +0530

    Update and rename p.py to Results checker(working).py

--- Results checker(working).py
+++ Results checker(working).py
@@ -1,5 +1,11 @@
 import time
 import pyautogui
+def enter():
+    pyautogui.keyDown("enter")
+    pyautogui.keyUp("enter") 
+def tab():
+    pyautogui.keyDown("tab")
+    pyautogui.keyUp("tab") 
 def slow_typing(element, text):
     for character in text: 
         element.send_keys(character)
@@ -9,10 +15,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 hall = input("Hall ticket.no=> ")
 dob = input("Date of birth(DDMMYYYY)=> ")
-#hall = "1234567890"
-#dob = "03102004"
 driver = webdriver.Edge('msedgedriver.exe')
-import time
 time.sleep(6)
 driver.get("https://results.bie.ap.gov.in/AP_Second_Year_General.do")
 time.sleep(3)
@@ -23,14 +26,33 @@ for character in hall:
     pyautogui.keyDown(character)
     pyautogui.keyUp(character)
     time.sleep(0.3) 
-pyautogui.keyDown("tab")
-pyautogui.keyUp("tab") 
+tab() 
 time.sleep(2)
 for character in dob:
     pyautogui.keyDown(character)
     pyautogui.keyUp(character)
     time.sleep(1)
-pyautogui.keyDown("tab")
-pyautogui.keyUp("tab")
-pyautogui.keyDown("enter")
-pyautogui.keyUp("enter") 
+tab()
+enter()
+time.sleep(5)
+x=4
+while x>0:
+    tab()
+    x=x-1
+enter()
+time.sleep(5)
+y=10
+while y>0:
+    tab()
+    y=y-1
+enter()
+time.sleep(10)
+name = (f"Marks of_{hall}")
+
+for x in name:
+    pyautogui.keyDown(x)
+    pyautogui.keyUp(x)
+    time.sleep(0.2)
+enter()
+time.sleep(10)
+driver.close()
